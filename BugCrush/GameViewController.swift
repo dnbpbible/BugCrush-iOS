c1edfe59ba6778e9cb451df6e0d3a7d98098c7b0
Paul E. Bible <paul_bible@daveandbusters.com>
2018-05-14 08:44:53 -0500

    Completed code conversion

--- BugCrush/GameViewController.swift
+++ BugCrush/GameViewController.swift
@@ -8,48 +8,173 @@
 
 import UIKit
 import SpriteKit
-import GameplayKit
+import AVFoundation
 
 class GameViewController: UIViewController {
 
+    // MARK: Properties
+    var scene: GameScene!
+    var level: Level!
+    
+    var movesLeft = 0
+    var score = 0
+    var tapGestureRecognizer: UITapGestureRecognizer!
+    var currentLevelNum = 1
+    
+    lazy var backgroundMusic: AVAudioPlayer? = {
+        guard let url = Bundle.main.url(forResource: "Mining by Moonlight", withExtension: "mp3") else {
+            return nil
+        }
+        do {
+            let player = try AVAudioPlayer(contentsOf: url)
+            player.numberOfLoops = -1
+            return player
+        } catch {
+            return nil
+        }
+    }()
+    
+    // MARK: IBOutlets
+    @IBOutlet weak var gameOverPanel: UIImageView!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var movesLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var shuffleButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+        setupLevel(number: currentLevelNum)
+        backgroundMusic?.play()
     }
 
+    func setupLevel(number levelNumber: Int) {
+        let skView = view as! SKView
+        skView.isMultipleTouchEnabled = false
+        
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .aspectFill
+        scene.addTiles()
+        scene.swipeHandler = handleSwipe
+        
+        gameOverPanel.isHidden = true
+        shuffleButton.isHidden = true
+        
+        skView.presentScene(scene)
+        beginGame()
+    }
+    
+    // MARK: IBActions
+    @IBAction func shuffleButtonPressed(_: AnyObject) {
+        shuffle()
+        decrementMoves()
+    }
+
+    // MARK: View Controller functions
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
     override var shouldAutorotate: Bool {
         return true
     }
-
+    
+    func beginGame() {
+        movesLeft = level.maximumMoves
+        score = 0
+        updateLabels()
+        level.resetComboMultiplier()
+        scene.animateBeginGame {
+            self.shuffleButton.isHidden = false
+        }
+        shuffle()
+    }
+    
+    func shuffle() {
+        scene.removeAllCookeSprites()
+        let newCookies = level.shuffle()
+        scene.addSprites(for: newCookies)
+    }
+    
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
+        return [.portrait, .portraitUpsideDown]
+    }
+    
+    func handleSwipe(_ swap: Swap) {
+        view.isUserInteractionEnabled = false
+        if level.isPossibleSwap(swap) {
+            level.performSwap(swap)
+            scene.animate(swap, completion: handleMatches)
         } else {
-            return .all
+            scene.animateInvalidSwap(swap) {
+                self.view.isUserInteractionEnabled = true
+            }
         }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
+    
+    func handleMatches() {
+        let chains = level.removeMatches()
+        if chains.count == 0 {
+            beginNextTurn()
+            return
+        }
+        
+        scene.animateMatchedCookies(for: chains) {
+            for chain in chains {
+                self.score += chain.score
+            }
+            self.updateLabels()
+            let columns = self.level.fillHoles()
+            self.scene.animateFallingCookies(in: columns) {
+                let columns = self.level.topUpCookies()
+                self.scene.animateNewCookies(in: columns) {
+                    self.handleMatches()
+                }
+            }
+        }
     }
-
-    override var prefersStatusBarHidden: Bool {
-        return true
+    
+    func beginNextTurn() {
+        level.detectPossibleSwaps()
+        view.isUserInteractionEnabled = true
+        decrementMoves()
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(format: "%ld", level.targetScore)
+        movesLabel.text = String(format: "%ld", movesLeft)
+        scoreLabel.text = String(format: "%ld", score)
+    }
+    
+    func decrementMoves() {
+        movesLeft -= 1
+        updateLabels()
+        if score >= level.targetScore {
+            gameOverPanel.image = UIImage(named: "LevelComplete")
+            currentLevelNum = currentLevelNum < numLevels ? currentLevelNum + 1 : 1
+            showGameOver()
+        } else if movesLeft == 0 {
+            gameOverPanel.image = UIImage(named: "GameOver")
+            showGameOver()
+        }
+    }
+    
+    func showGameOver() {
+        gameOverPanel.isHidden = false
+        scene.isUserInteractionEnabled = false
+        shuffleButton.isHidden = true
+        
+        scene.animateGameOver {
+            self.tapGestureRecognizer = UITapGestureRecognizer(
+                target: self, action: #selector(self.hideGameOver))
+            self.view.addGestureRecognizer(self.tapGestureRecognizer)
+        }
+    }
+    
+    @objc func hideGameOver() {
+        view.removeGestureRecognizer(tapGestureRecognizer)
+        tapGestureRecognizer = nil
+        gameOverPanel.isHidden = true
+        scene.isUserInteractionEnabled = true
+        setupLevel(number: currentLevelNum)
     }
 }
